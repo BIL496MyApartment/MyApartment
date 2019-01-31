e204d454a3500d8e9b39fc07a3efd51879fca743
Mertcan KARACA <mckaraca01@gmail.com>
2019-02-01 01:45:31 +0300

    survey butonu aktiflestirmesi

--- app/src/main/java/com/bil495calendear/bitirmeprojesi/MainActivity.java
+++ app/src/main/java/com/bil495calendear/bitirmeprojesi/MainActivity.java
@@ -3,6 +3,8 @@ package com.bil495calendear.bitirmeprojesi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mButton = (Button) findViewById(R.id.button3);//gunceldurumlar buttoni aktiflestirme
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+    
     }
 
     @Override
