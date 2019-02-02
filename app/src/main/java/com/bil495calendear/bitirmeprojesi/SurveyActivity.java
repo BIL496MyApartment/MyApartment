f1bed91d3bb0d42c23fcb486f7a6bcd83fd96d99
Mertcan KARACA <mckaraca01@gmail.com>
2019-02-02 22:54:04 +0300

    Add files via upload

---
+++ app/src/main/java/com/bil495calendear/bitirmeprojesi/SurveyActivity.java
@@ -0,0 +1,14 @@
+package com.bil495calendear.bitirmeprojesi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SurveyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+    }
+}
