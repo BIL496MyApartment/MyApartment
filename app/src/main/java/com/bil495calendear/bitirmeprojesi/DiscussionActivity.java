fc75a889926237af057be892b9dde4b6247b1508
egetan <36420766+egetan@users.noreply.github.com>
2019-02-03 18:54:21 +0300

    adding forgotten file

---
+++ app/src/main/java/com/bil495calendear/bitirmeprojesi/DiscussionActivity.java
@@ -0,0 +1,12 @@
+package com.bil495calendear.bitirmeprojesi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DiscussionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discussion);
+    }
+}
